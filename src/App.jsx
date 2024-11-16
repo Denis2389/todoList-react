9a38a3b9a118916387920401d937888be2aceb61
Denis2389 <bereznyakdenis2003@gmail.com>
2024-11-16 12:06:35 +0200

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
-import CurrencyConverter from './CurrencyConverter'
+import { useState } from "react"
 
 const App = () => {
+  
+  const [tasks, setTasks] = useState([])
+  const [newTask, setNewTask] = useState('')
+
+  const addTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, newTask.trim()])
+      setNewTask('')
+    }
+  }
+
   return (
-    <CurrencyConverter />
+    <div>
+      <input onChange={(e) => setNewTask(e.target.value)} type="text" value={newTask}/>
+      <button onClick={addTask}>Add</button>
+
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
