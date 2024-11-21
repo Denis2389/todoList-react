cc25f061c481cb945f41d1757c0044390db27b90
Denis2389 <bereznyakdenis2003@gmail.com>
2024-11-21 15:25:37 +0200

    api train

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,44 @@
-import { Routes, Route } from 'react-router-dom'
-import { Home } from './component/Home'
-import { LoginPage } from './component/LoginPage'
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const App = () => {
 
+  const [datas, setDatas] = useState([])
+  const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+      async function fetchData() {
+        try {
+          setLoading(true)
+          const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+          setDatas(response.data)
+          setLoading(false)
+        } catch (error) {
+          console.error(error)
+          setLoading(false)
+        } 
+      }
+
+      fetchData()
+    }, [])  
+
   return (
     <div>
-      <Routes>
-        <Route path='/' element={<LoginPage />} />
-        <Route path='/home' element={<Home />} />
-      </Routes>
+      <h1>Api info</h1>
+      {loading ? (
+        <p>Loading</p>
+      ) : datas.length > 0 ? (
+        <ul>
+          {datas.map((data) => (
+            <li key={data.id}>
+              <h3>{data.title}</h3>
+              <p>{data.body}</p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>Нет данных</p>
+      )}
     </div>
   );
 }
